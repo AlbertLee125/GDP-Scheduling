0f91ef82cc2f2cf4b3f196e6b27009273057f909
Albert Lee <lee4382@purdue.edu>
2025-04-22 17:09:21 -0400

    Increase time limit for model benchmarking and adjust scheduling data range
    - Updated the time limit for the solver from 900 seconds to 7200 seconds.
    - Changed the scheduling data range from 1-10 to 9-10 for benchmarking.
    - Modified output filenames for benchmark results to reflect the new scheduling data range.
    - Added new benchmark results for scheduling data 7 to 9 in both JSON and TXT formats.

--- main_benchmark.py
+++ main_benchmark.py
@@ -19,11 +19,11 @@ from model.single_unit_sequencing_ts import (
 def benchmark_models():
     # Create a solver instance and set a time limit.
     solver = pyo.SolverFactory("gurobi")
-    solver.options["time_limit"] = 900  # set time limit for each solve
+    solver.options["time_limit"] = 7200  # set time limit for each solve
     solver.options["Threads"] = 1 # force single-threaded
 
     # Define scheduling data IDs (for example, 1 to 10)
-    scheduling_data_list = list(range(1, 11))
+    scheduling_data_list = list(range(9, 11))
 
     # MINLP models (manually reformulated into MINLP)
     minlp_models = {
@@ -114,7 +114,7 @@ def benchmark_models():
     os.makedirs(results_dir, exist_ok=True)
 
     # Write the summary to a text file.
-    txt_file = os.path.join(results_dir, "benchmark_results_gp_900.txt")
+    txt_file = os.path.join(results_dir, "benchmark_results_gp_9to10_900.txt")
     with open(txt_file, "w") as f:
         f.write("{:<9} | {:<30} | {:<12} | {:>10} | {:>10} | {:<}\n".format(*header))
         f.write("-" * 90 + "\n")
@@ -123,7 +123,7 @@ def benchmark_models():
     print(f"\nBenchmark summary written to {txt_file}")
 
     # Write the summary to a JSON file.
-    json_file = os.path.join(results_dir, "benchmark_results_gp_900.json")
+    json_file = os.path.join(results_dir, "benchmark_results_gp_9to10_900.json")
     json_results = []
     for s, mname, ttype, t, obj, term in results:
         json_results.append({
