761e1dfa484e991096036f447d6df42bd7f63349
Albert Lee <lee4382@purdue.edu>
2025-04-29 23:21:23 -0400

    Borrowed the code from the Pyomo.GDP

---
+++ strip_packing/strip_packing_8rect.py
@@ -0,0 +1,80 @@
+import pyomo.environ as pyo
+
+def build_rect_strip_packing_model():
+    """Build the strip packing model."""
+    model = pyo.ConcreteModel(name="Rectangles strip packing")
+    model.rectangles = pyo.Set(ordered=True, initialize=[0, 1, 2, 3, 4, 5, 6, 7])
+
+    # Width and Length of each rectangle
+    model.rect_width = pyo.Param(
+        model.rectangles, initialize={0: 3, 1: 3, 2: 2, 3: 2, 4: 3, 5: 5, 6: 7, 7: 7}
+    )
+    # parameter indexed by each rectangle
+    # same as height?
+    model.rect_length = pyo.Param(
+        model.rectangles, initialize={0: 4, 1: 3, 2: 2, 3: 2, 4: 3, 5: 3, 6: 4, 7: 4}
+    )
+
+    model.strip_width = pyo.Param(initialize=10, doc="Available width of the strip")
+
+    # upperbound on length (default is sum of lengths of rectangles)
+    model.max_length = pyo.Param(
+        initialize=sum(model.rect_length[i] for i in model.rectangles),
+        doc="maximum length of the strip (if all rectangles were arranged "
+        "lengthwise)",
+    )
+
+    # x (length) and y (width) coordinates of each of the rectangles
+    model.x = pyo.Var(
+        model.rectangles,
+        bounds=(0, model.max_length),
+        doc="rectangle corner x-position (position across length)",
+    )
+
+    def w_bounds(m, i):
+        return (0, m.strip_width - m.rect_width[i])
+
+    model.y = pyo.Var(
+        model.rectangles,
+        bounds=w_bounds,
+        doc="rectangle corner y-position (position down width)",
+    )
+
+    model.strip_length = pyo.Var(within=pyo.NonNegativeReals, doc="Length of strip required.")
+
+    def rec_pairs_filter(model, i, j):
+        return i < j
+
+    model.overlap_pairs = pyo.Set(
+        initialize=model.rectangles * model.rectangles,
+        dimen=2,
+        filter=rec_pairs_filter,
+        doc="set of possible rectangle conflicts",
+    )
+
+    @model.Constraint(model.rectangles)
+    def strip_ends_after_last_rec(model, i):
+        return model.strip_length >= model.x[i] + model.rect_length[i]
+
+    model.total_length = pyo.Objective(expr=model.strip_length, doc="Minimize length")
+
+    @model.Disjunction(
+        model.overlap_pairs,
+        doc="Make sure that none of the rectangles on the strip overlap in "
+        "either the x or y dimensions.",
+    )
+    def no_overlap(m, i, j):
+        return [
+            m.x[i] + m.rect_length[i] <= m.x[j],
+            m.x[j] + m.rect_length[j] <= m.x[i],
+            m.y[i] + m.rect_width[i] <= m.y[j],
+            m.y[j] + m.rect_width[j] <= m.y[i],
+        ]
+
+    return model
+
+if __name__ == "__main__":
+    model = build_rect_strip_packing_model()
+    pyo.TransformationFactory('gdp.hull').apply_to(model)
+    opt = pyo.SolverFactory('gurobi')
+    results = opt.solve(model, tee=True)
